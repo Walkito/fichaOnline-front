3eff80bd5020c4a63b52b9a0d1855a30e4008657
Edamatec <60098824+Edamatec@users.noreply.github.com>
2024-07-11 15:29:30 -0300

    Correção URL DA API 2

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,2 +1,2 @@
-export const API_PATH = "https://15.229.104.156:8080/api/";
-export const WEBSOCKET_PATH = "https://15.229.104.156:8080/ws";
+export const API_PATH = "https://ec2-15-229-104-156.sa-east-1.compute.amazonaws.com:8080/api/";
+export const WEBSOCKET_PATH = "https://ec2-15-229-104-156.sa-east-1.compute.amazonaws.com:8080/ws";
