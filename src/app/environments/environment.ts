28e2e648974c9ec49393d653e24521aa02ac4fc1
Edamatec <60098824+Edamatec@users.noreply.github.com>
2024-07-11 14:34:37 -0300

    URL da API

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,2 +1,2 @@
-export const API_PATH = "http://15.229.104.156:8080/api/";
-export const WEBSOCKET_PATH = "http://15.229.104.156:8080/ws";
+export const API_PATH = "https://15.229.104.156:8080/api/";
+export const WEBSOCKET_PATH = "https://15.229.104.156:8080/ws";
