d3bc9c2842b5a9d562e6b44a82af867402630048
Edamatec <60098824+Edamatec@users.noreply.github.com>
2024-07-15 10:39:07 -0300

    Versao Alpha

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,2 +1,2 @@
-export const API_PATH = "https://ec2-15-229-104-156.sa-east-1.compute.amazonaws.com:8080/api/";
-export const WEBSOCKET_PATH = "https://ec2-15-229-104-156.sa-east-1.compute.amazonaws.com:8080/ws";
+export const API_PATH = "http://ec2-15-229-104-156.sa-east-1.compute.amazonaws.com:8080/api/";
+export const WEBSOCKET_PATH = "http://ec2-15-229-104-156.sa-east-1.compute.amazonaws.com:8080/ws";
